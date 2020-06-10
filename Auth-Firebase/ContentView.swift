5ab8810454948f05a5d7b8e35da85f7965ac1afb
Annisa Nabila Nasution <annisanabila14@gmail.com>
2020-06-10 16:28:07 +0700

    Initial Commit

---
+++ Auth-Firebase/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Auth-Firebase
+//
+//  Created by Annisa Nabila Nasution on 10/06/20.
+//  Copyright © 2020 Annisa Nabila Nasution. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
